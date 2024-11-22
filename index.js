df9fe9825255b52a915b8f182c453c51de152b49
nunof083 <nunof083@gmail.com>
2024-11-22 10:32:41 +0100

    translating website to app 2

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 
 const createWindow = () => {
     const win = new BrowserWindow({
+        icon: path.join(__dirname, './icon.png'),
         width: 800,
         height: 600
     });
 
-    win.loadFile('./public/index.html');
+    win.loadFile('./public/pages/index.html');
 }
 
 app.whenReady().then(() => {
